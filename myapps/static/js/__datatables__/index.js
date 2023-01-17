029e51bb32d38c40dc00b17c95254ad3f7a82881
SatoTake2019 <kiri90hari7352@gmail.com>
2023-01-17 13:25:44 +0900

    modify init for workaround

--- myapps/static/js/__datatables__/index.js
+++ myapps/static/js/__datatables__/index.js
@@ -2,9 +2,27 @@ import DataTable from "datatables.net-bs5";
 
 // export is for test
 export function initTable() {
-    let table = new DataTable('#example', {paging: false});
+    let table;
+    const options = { paging: false };
+
+    if (globalThis.hasOwnProperty('test')) {
+        /*
+            This initialization expression passes Jest unit tests,
+            but webpack-built bundle file gives an error in the browser.
+         */
+        let DataTableFunc = new DataTable(null, null);
+        table = DataTableFunc.Api('#example', options);
+    }
+    else {
+        /*
+            This initialization expression will fail in Jest test,
+            but webpack-built bundle file goes well on browser.
+         */
+        table = new DataTable('#example', options);
+    }
     return table;
 }
+
 document.addEventListener('DOMContentLoaded', function () {
     let table = initTable();
 });
