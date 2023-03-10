abe73ef2d4dd1ec7020e46e50c9e2588ebe73e67
SatoTake2019 <kiri90hari7352@gmail.com>
2023-03-10 09:17:50 +0900

    success but ライブラリソースをコピーして、import文を書き換えるというひどい回避策だが、rowReorder拡張機能はロードすることができた。

--- myapps/static/js/__datatables__/__tests__/index.test.js
+++ myapps/static/js/__datatables__/__tests__/index.test.js
@@ -1,12 +1,18 @@
+import { readFileSync } from "fs";
 import { initTable } from "../index";
 
-describe("test Datatable initialization", () => {
-    test("test one module", () => {
-        document.addEventListener('DOMContentLoaded', function () {
-            let table = initTable();
-        });
 
-        const event = new Event('DOMContentLoaded');
-        document.dispatchEvent(event);
+describe("test Datatable initialization", () => {
+    beforeEach(() => {
+        document.body.innerHTML = readFileSync("__tests__/example.html", { encoding: "utf-8" });
+    });
+    test("test one module", () => {
+
+        const table = initTable();
+
+        expect(table).not.toBeUndefined();
+
+
+        const table_element = document.getElementById('example');
     });
 });
--- myapps/static/js/__datatables__/index.js
+++ myapps/static/js/__datatables__/index.js
@@ -1,10 +1,25 @@
-import DataTable from "datatables.net-bs5";
+import DforJEST from "./datatables.bootstrap5";  // inside my project
+import './dataTables.rowReorder';                // inside my project
+import DforBROWSER from "datatables.net-bs5";   // from ./node_modules/...
+import "datatables.net-rowreorder-bs5";         // from ./node_modules/...
+
+let DataTable;
+DataTable = (process.env['NODE_ENV'] === 'test') ? DforJEST : DforBROWSER;
 
 // export is for test
 export function initTable() {
-    let table = new DataTable('#example', {paging: false});
+    const options = {
+        rowReorder: {
+            snapX: true,        // ドラッグした時に横方向に動かないようにする。
+            update: false        //  ドロップした時に、自動再描画する
+        },
+    };
+    let table = new DataTable('#example', options);
     return table;
 }
-document.addEventListener('DOMContentLoaded', function () {
-    let table = initTable();
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    const datatable = initTable();
+    console.log(datatable.rowReorder);
+    
 });
